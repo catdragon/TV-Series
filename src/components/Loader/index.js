ccf6a3f256e5b0a9d0ed03002f0a822d09606974
Bobby Nichols <rnichol2@cisco.com>
2020-05-01 14:57:19 -0400

    First commit

---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import loaderSrc from '../../assets/loading.gif';
+
+const Loader = props => (
+    <div>
+        <img style={{width: 75}} alt="loading" src={loaderSrc} />
+    </div>
+);
+
+export default Loader;
